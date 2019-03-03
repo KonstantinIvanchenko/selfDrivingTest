87663da271b5e2cacd60f0bd5e86af2733a36b69
konstiva <ikv.mstu@gmail.com>
2019-03-03 21:04:16 +0100

    Added data splitting into validation and training sets. Used Sk-learn

--- CamAutomatedDriveTest.py
+++ CamAutomatedDriveTest.py
@@ -7,6 +7,7 @@ from keras.optimizers import Adam
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split as tts
 
 import cv2
 import pandas as pd
@@ -70,3 +71,32 @@ plt.bar(center, hist, width=0.05)
 plt.plot((np.min(data['steering']), np.max(data['steering'])),
         (samples_per_bin, samples_per_bin))
 plt.show()
+
+
+def load_img_steering(datadir, df):
+    image_path = []
+    steering = []
+    for i in range(len(data)):
+        indexed_data = data.iloc[i]
+        center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+        # use strip to remove all spaces in the path (there are no but..)
+        image_path.append(os.path.join(datadir, center.strip()))
+        steering.append(float(indexed_data[3]))
+    image_paths = np.asarray(image_path)
+    steerings = np.asarray(steering)
+    return image_paths, steerings
+
+
+image_paths, steerings = load_img_steering(datadir+'/IMG', data)
+
+X_train, X_valid, y_train, y_valid = tts(image_paths, steerings, test_size=0.2, random_state=5)
+
+print('Training samples: {}\nValid Samples: {}'.format(len(X_train), len(X_valid)))
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+axes[0].hist(y_train, bins=num_bins, width=0.05, color='blue')
+axes[0].set_title('Training set')
+axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
+axes[1].set_title('Validation set')
+
+plt.show(fig)
